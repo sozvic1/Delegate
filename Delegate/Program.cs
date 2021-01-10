e9dac33cc21fea04ba8b5675ac5907f46619cc3e
sozvi <sozvi@DESKTOP-J2G9TPS>
2021-01-10 22:17:12 +0200

    Add project files.

---
+++ Delegate/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Delegate
+{
+    delegate int MyDelegate(int a, int b, int c);
+    delegate int MathemathicDelegate(int a, int b);
+    delegate int Del();
+    delegate double ArrayDelegate(Del[] a);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region First task
+            MyDelegate myDelegate = (int x, int y, int c) => (x + y + c) / 3;
+            Console.WriteLine($"Среднее арифметическое {myDelegate(2, 3, 4)}");
+            #endregion
+            #region Second task
+            Console.WriteLine("Введите математическое действие: ");
+            var sign = Convert.ToChar(Console.ReadLine());
+
+            MathemathicDelegate add = (int x, int y) => (x + y);
+            MathemathicDelegate minus = (int x, int y) => (x - y);
+            MathemathicDelegate multiply = (int x, int y) => (x * y);
+            MathemathicDelegate divide = (int x, int y) =>
+           {
+               if (y == 0) throw new ArgumentException();
+               return x / y;
+
+           };
+            ChoseOperation(sign);
+
+            static void Operation(int a, int b, MathemathicDelegate mathemathic)
+            {
+                var value = mathemathic(a, b);
+                Console.WriteLine(value);
+            }
+            void ChoseOperation(char znak)
+            {
+                switch (sign)
+                {
+                    case '+':
+                        Operation(5, 5, add);
+                        break;
+                    case '-':
+                        Operation(5, 5, minus);
+                        break;
+                    case '*':
+                        Operation(5, 5, multiply);
+                        break;
+                    case '/':
+                        Operation(5, 5, divide);
+                        break;
+                    default:
+                        Console.WriteLine("Нету такого действия");
+                        break;
+                }
+
+            }
+            #endregion
+            #region third task
+            static int GetRandom()
+            {
+                return new Random().Next(100);
+            }
+
+            Del[] arrayDelegate = new Del[10];
+            for (int i = 0; i < arrayDelegate.Length; i++)
+            {
+                arrayDelegate[i] = () => new Del(GetRandom).Invoke();
+            }
+            ArrayDelegate array = delegate (Del[] dels)
+            {
+                double sum = 0;
+                for (int i = 0; i < dels.Length; i++)
+                {
+                    sum += dels[i].Invoke();
+                }
+                return sum / dels.Length;
+            };
+
+            for (int i = 0; i < arrayDelegate.Length; i++)
+            {
+                Console.Write(arrayDelegate[i].Invoke() + " ");
+            }
+            Console.WriteLine($"Среднее число {array(arrayDelegate)}");
+            #endregion
+
+        }
+
+    }
+    }
+
---
+++ Timer/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            timer1.Interval = 500;
+         
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+           // timer1.Interval = 100;            
+            i++;
+            timer1.Start();
+            timer1.Enabled = true;
+            textBox1.Text = i.ToString(); ;
+        }
+
+        private void Start(object sender, EventArgs e)
+        {
+            
+            timer1.Enabled = true;
+        }
+
+        private void Stop(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void Reset(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            textBox1.Clear();
+            i = 0;
+        }
+    }
+}
